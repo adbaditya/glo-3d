098117435a6facbabc6b0c063e136a861fa6b066
Aditya Bajaj <adb.aditya@gmail.com>
2024-11-14 10:44:18 +0530

    removed some complex cors in server.js

--- server.js
+++ server.js
@@ -6,29 +6,6 @@ require('dotenv').config();
 
 const app = express();
 
-// Comprehensive CORS configuration
-app.use(cors({
-  origin: ['https://app.hubspot.com', 'https://glo-3d.vercel.app', 'http://localhost:3000'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
-  credentials: true,
-  maxAge: 86400 // 24 hours
-}));
-
-// Set additional security headers
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  res.header('Access-Control-Allow-Credentials', true);
-  
-  // Handle OPTIONS method
-  if (req.method === 'OPTIONS') {
-      return res.sendStatus(200);
-  }
-  next();
-});
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Middleware
